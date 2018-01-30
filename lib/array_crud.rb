96bd98a5dc3f126f868eadc71626f43ad9ccbc63
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 20:37:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,9 +27,10 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  x = array.count
+  array[x-1]
 end
